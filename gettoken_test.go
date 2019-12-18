099bf8f6a3b85fc79eeecfa5dbcc6942efde85e8
Alberto Bregliano <alberto.bregliano@pm.me>
2019-12-18 16:26:55 +0100

    testing modified

--- gettoken_test.go
+++ gettoken_test.go
@@ -2,19 +2,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package token
+package token_test
 
 import (
 	"context"
 	"testing"
 
 	"github.com/axamon/hashstring"
+	"github.com/axamon/token"
 )
 
 func TestCheckLocalCredentials(t *testing.T) {
 	type args struct {
 		ctx context.Context
-		c   *Credentials
+		c   *token.Credentials
 	}
 	tests := []struct {
 		name    string
@@ -23,16 +24,15 @@ func TestCheckLocalCredentials(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{name: "primo", args: 
-		args{ctx: context.TODO(),
-			c: &Credentials{
-				User: "pippo", 
+		{name: "primo", args: args{ctx: context.TODO(),
+			c: &token.Credentials{
+				User:     "pippo",
 				Hashpass: hashstring.Md5Sum("pippo")}},
-		want: true, wantErr: false},
+			want: true, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := CheckLocalCredentials(tt.args.ctx, tt.args.c)
+			got, err := token.CheckLocalCredentials(tt.args.ctx, tt.args.c)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CheckLocalCredentials() error = %v, wantErr %v", err, tt.wantErr)
 				return
